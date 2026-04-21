a919ac2492847310503afb5be9b5cca2854d9dd4
Bigpig8787 <F74126369@gs.ncku.edu.tw>
2026-04-21 23:39:04 +0800

    Update eval_multiview.py

--- eval_multiview.py
+++ eval_multiview.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     bart = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
     model = MultiViewConformerTranslator(bart, d_model=512, n_filters=40, temporal_kernel=25,
                                          pool_stride=10, tokens_per_view=100, n_heads=8,
-                                         n_encoder_layers=4, n_global_layers=3, decoder_embedding_size=1024)
+                                         n_encoder_layers=2, n_global_layers=2, decoder_embedding_size=1024)
     model.load_state_dict(torch.load(args['checkpoint_path'], map_location=device))
     model.to(device)
 
